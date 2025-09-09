cb1b5ccaede6f50df220017b34b2f8b0a42182c1
nicolelinh444 <nicolelinh444@gmail.com>
2025-09-09 12:30:08 -0700

    completed form on SavedCountries page and styling

--- version-1/src/components/CountryCard.jsx
+++ version-1/src/components/CountryCard.jsx
@@ -1,7 +1,7 @@
 function CountryCard({ country }) {
     return (
         <div className="countries-card">
-        <img src={country.flags.png} alt={`flag of ${country.name.common}`} width="300" height="200"/>
+        <img src={country.flags.png} alt={`flag of ${country.name.common}`}/>
         <div className="countries-text">
         <h2>{country.name.common}</h2>
         <p><strong>Population: </strong>{country.population}</p>
--- version-1/src/pages/SavedCountries.jsx
+++ version-1/src/pages/SavedCountries.jsx
@@ -1,6 +1,84 @@
 // pages/SavedCountries.jsx
+import { useState } from "react";
+
 function SavedCountries() {
-    return <h1>Welcome to the Saved Countries Page</h1>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    country: "",
+    bio: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData, "formData");
+    setFormData({
+      name: "",
+      email: "",
+      country: "",
+      bio: "",
+    });
   }
 
-  export default SavedCountries;
+  return (
+    <div className="saved-countries-page">
+    <h2>My Saved Countries</h2>
+    <div className="form-container">
+      <h2>My Profile</h2>
+      <br />
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          placeholder="Full Name"
+          type="text"
+          name="name"
+          id="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          required
+          placeholder="Email"
+          type="email"
+          name="email"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          required
+          placeholder="Country"
+          type="text"
+          name="country"
+          id="country"
+          value={formData.country}
+          onChange={handleChange}
+        />
+        <br />
+        <textarea
+        rows="15"
+        cols="60"
+        placeholder="Bio"
+        type="text"
+        name="bio"
+        id="bio"
+        value={formData.bio}
+        onChange={handleChange}/>
+        <br /><br />
+        <button className="button" type="Submit">
+            Submit
+          </button>
+      </form>
+    </div>
+    </div>
+  );
+}
+
+export default SavedCountries;
