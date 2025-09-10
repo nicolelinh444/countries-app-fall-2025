984c4dc6d2fd44dc189044c3326954302598360b
nicolelinh444 <nicolelinh444@gmail.com>
2025-09-10 13:45:53 -0700

    alphabetized country names

--- version-1/src/components/CountryCard.jsx
+++ version-1/src/components/CountryCard.jsx
@@ -5,12 +5,12 @@ function CountryCard({ country }) {
         <Link to={`/country-detail/${country.name.common}`}>
         <div className="countries-card">
         <img src={country.flags.png} alt={`flag of ${country.name.common}`}/>
-        <div className="countries-text">
+        <span className="countries-text">
         <h2>{country.name.common}</h2>
         <p><strong>Population: </strong>{country.population}</p>
         <p><strong>Region: </strong>{country.region}</p>
         <p><strong>Capital: </strong>{country.capital}</p>
-        </div>
+        </span>
         </div>
         </Link>
     )
--- version-1/src/pages/Home.jsx
+++ version-1/src/pages/Home.jsx
@@ -2,9 +2,14 @@
 import CountryCard from "../components/CountryCard";
 
 function Home({ countriesData }) {
+    const sortCountries = [...countriesData].sort((a, b) =>
+        a.name.common.localeCompare(b.name.common)
+    );
+
     return (
+
         <div className="countries-list">
-            {countriesData.map((country, index) => (
+            {sortCountries.map((country, index) => (
             <CountryCard key={index} country={country} />
         ))}
         </div>
