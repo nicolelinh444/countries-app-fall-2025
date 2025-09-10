fea587a3e7d90defa39f91fc1fd1dc03393bd665
nicolelinh444 <nicolelinh444@gmail.com>
2025-09-10 13:55:50 -0700

    added commas to population

--- version-1/src/components/CountryCard.jsx
+++ version-1/src/components/CountryCard.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom"
 
+function PopulationWithCommas ({ value }) {
+    return <>{value.toLocaleString("en-US")}</>
+}
+
 function CountryCard({ country }) {
     return (
         <Link to={`/country-detail/${country.name.common}`}>
@@ -7,7 +11,7 @@ function CountryCard({ country }) {
         <img src={country.flags.png} alt={`flag of ${country.name.common}`}/>
         <span className="countries-text">
         <h2>{country.name.common}</h2>
-        <p><strong>Population: </strong>{country.population}</p>
+        <p><strong>Population: </strong><PopulationWithCommas value={country.population}/></p>
         <p><strong>Region: </strong>{country.region}</p>
         <p><strong>Capital: </strong>{country.capital}</p>
         </span>
