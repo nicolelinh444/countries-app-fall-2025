d4ae844ded5ad8d7a35a27a6c3cb4b2319e6a861
nicolelinh444 <nicolelinh444@gmail.com>
2025-11-17 11:20:44 -0800

    version 4 pseudocode

--- version-4/server/src/config.js
+++ version-4/server/src/config.js
@@ -1,5 +1,4 @@
-const config = {
-  databaseUrl: "YOUR DATABASE CONNECTION STRING GOES HERE",
+const config = {"postgresql://neondb_owner:npg_f89iewhjKNql@ep-cool-king-afdtd1f1-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 };
 
 export default config;
